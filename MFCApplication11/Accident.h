e70d44d0fe256cb3d22054261eb901c8c17cb6b4
yungoonkim <103237653+yungoonkim@users.noreply.github.com>
2022-04-18 10:33:30 +0900

    Add files via upload

---
+++ MFCApplication11/Accident.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+#include <sstream> //string stream
+#include <fstream> //file stream
+#include <string>
+
+using namespace std;
+
+
+class Accident
+{
+public:
+	CString sido;
+	CString sigoongu;
+	CString cnt[11];
+
+};
